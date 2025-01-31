65df8c4886e22a78eacfea7cbd11238be22a8965
bttobi <bttobi123@gmail.com>
2025-01-31 14:07:43 +0100

    fixed mason issue - double calling

--- lua/bttobi/plugins/lsp/lspconfig.lua
+++ lua/bttobi/plugins/lsp/lspconfig.lua
@@ -10,14 +10,70 @@ return {
 	},
 	config = function()
 		local lspconfig = require("lspconfig")
+
 		local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+		local map = vim.keymap
+		vim.api.nvim_create_autocmd("LspAttach", {
+			group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+			callback = function(ev)
+				local opts = { buffer = ev.buf, silent = true }
+
+				opts.desc = "Show LSP references"
+				map.set("n", "gR", "<cmd>Telescope lsp_references<CR>", opts)
+
+				opts.desc = "Go to declaration"
+				map.set("n", "gD", vim.lsp.buf.declaration, opts)
+
+				opts.desc = "Show LSP definitions"
+				map.set("n", "gd", "<cmd>Telescope lsp_definitions<CR>", opts)
+
+				opts.desc = "Show LSP implementations"
+				map.set("n", "gi", "<cmd>Telescope lsp_implementations<CR>", opts)
+
+				opts.desc = "Show LSP type definitions"
+				map.set("n", "gt", "<cmd>Telescope lsp_type_definitions<CR>", opts)
+
+				opts.desc = "See available code actions"
+				map.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+
+				opts.desc = "Smart rename"
+				map.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+
+				opts.desc = "Show buffer diagnostics"
+				map.set("n", "<leader>D", "<cmd>Telescope diagnostics bufnr=0<CR>", opts)
+
+				opts.desc = "Show line diagnostics"
+				map.set("n", "<leader>d", vim.diagnostic.open_float, opts)
+
+				opts.desc = "Go to previous diagnostic"
+				map.set("n", "[d", vim.diagnostic.goto_prev, opts)
+
+				opts.desc = "Go to next diagnostic"
+				map.set("n", "]d", vim.diagnostic.goto_next, opts)
+
+				opts.desc = "Show documentation for what is under cursor"
+				map.set("n", "K", vim.lsp.buf.hover, opts)
+
+				opts.desc = "Restart LSP"
+				map.set("n", "<leader>rs", ":LspRestart<CR>", opts)
+			end,
+		})
+
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		-- Ensure Mason is set up first before LSPConfig
+		local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
+		for type, icon in pairs(signs) do
+			local hl = "DiagnosticSign" .. type
+			vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+		end
+
 		require("mason").setup()
+
 		require("mason-lspconfig").setup({
+			automatic_installation = true,
 			ensure_installed = {
-				"ts_ls", -- FIXED (was "ts_ls")
+				"ts_ls",
 				"html",
 				"cssls",
 				"tailwindcss",
@@ -27,45 +83,49 @@ return {
 				"prismals",
 				"pyright",
 			},
-		})
-
-		-- Setup LSP servers properly after Mason is initialized
-		require("mason-lspconfig").setup_handlers({
-			function(server_name)
-				lspconfig[server_name].setup({
-					capabilities = capabilities,
-				})
-			end,
-			["jdtls"] = function()
-				require("java").setup()
-				lspconfig["jdtls"].setup({})
-			end,
-			["emmet_ls"] = function()
-				lspconfig["emmet_ls"].setup({
-					capabilities = capabilities,
-					filetypes = {
-						"html",
-						"typescriptreact",
-						"javascriptreact",
-						"css",
-						"sass",
-						"scss",
-						"less",
-						"svelte",
-					},
-				})
-			end,
-			["lua_ls"] = function()
-				lspconfig["lua_ls"].setup({
-					capabilities = capabilities,
-					settings = {
-						Lua = {
-							diagnostics = { globals = { "vim" } },
-							completion = { callSnippet = "Replace" },
+			handlers = {
+				function(server_name)
+					lspconfig[server_name].setup({
+						capabilities = capabilities,
+					})
+				end,
+				jdtls = function()
+					require("java").setup()
+					lspconfig["jdtls"].setup({})
+				end,
+				["emmet_ls"] = function()
+					-- configure emmet language server
+					lspconfig["emmet_ls"].setup({
+						capabilities = capabilities,
+						filetypes = {
+							"html",
+							"typescriptreact",
+							"javascriptreact",
+							"css",
+							"sass",
+							"scss",
+							"less",
+							"svelte",
 						},
-					},
-				})
-			end,
+					})
+				end,
+				["lua_ls"] = function()
+					-- configure lua server (with special settings)
+					lspconfig["lua_ls"].setup({
+						capabilities = capabilities,
+						settings = {
+							Lua = {
+								diagnostics = {
+									globals = { "vim" },
+								},
+								completion = {
+									callSnippet = "Replace",
+								},
+							},
+						},
+					})
+				end,
+			},
 		})
 	end,
 }
